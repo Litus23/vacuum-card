3c847a15076d829ab84ab37ef4ae445f75895fa1
Litus23 <litus23@gmail.com>
2020-08-15 12:56:21 +0200

    Add files via upload
    In Catalan

---
+++ src/translations/ca.js
@@ -0,0 +1,45 @@
+{
+  "status": {
+    "Cleaning": "Netejant",
+    "Paused": "En pausa",
+    "Idle": "Inactiu",
+    "Charging": "Carregant",
+    "Returning home": "Tornant a la base"
+  },
+  "source": {
+    "Gentle": "Delicat",
+    "Silent": "Silenciós",
+    "Standard": "Standard",
+    "Medium": "Mig",
+    "Turbo": "Turbo"
+  },
+  "common": {
+    "name": "Vacuum Card",
+    "description": "Vacuum card et permet controlar el teu robot aspirador.",
+    "start": "Conençar",
+    "continue": "Continuar",
+    "pause": "Pausar",
+    "stop": "Aturar",
+    "return_to_base": "Tornar a la base",
+    "locate": "Trobar"
+  },
+  "error": {
+    "missing_entity": "Es necessita especificar una entitat!"
+  },
+  "editor": {
+    "entity": "Entitat (Requerit)",
+    "map": "Map Camera (Opcional)",
+    "image": "Imatge (Opcional)",
+    "compact_view": "Vista compacta",
+    "compact_view_aria_label_on": "Activar vista compacta",
+    "compact_view_aria_label_off": "Desactivar vista compacta",
+    "show_name": "Nom a mostrar",
+    "show_name_aria_label_on": "Mostrar nom",
+    "show_name_aria_label_off": "Ocultar nom",
+    "show_toolbar": "Mostrar barra d'eines",
+    "show_toolbar_aria_label_on": "Activar la barra d'eines",
+    "show_toolbar_aria_label_off": "Desactivar la barra d'eines",
+    "code_only_note": "Nota: La configuració de les accions i estadístiques està únicament disponible a través de l'Editor de Codi."
+  }
+}
+© 2020 GitHub, Inc.
